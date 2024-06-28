03a800437ba6470674029af18083804d407bcf57
Amirreza Hosseini <hosseiniseyyedamirreza2003@gmial.com>
2024-06-28 17:43:34 +0430

    connecting to mysql server

--- laleh_store/settings.py
+++ laleh_store/settings.py
@@ -105,8 +105,11 @@ WSGI_APPLICATION = 'laleh_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'laleh_store',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'seyyedamirreza1381'
     }
 }
 
