143d1f38f688418b2a2b482a4ce6f7f08ebef600
Amirreza Hosseini <hosseiniseyyedamirreza2003@gmial.com>
2024-06-28 17:27:18 +0430

    changing permissions

--- shop/views.py
+++ shop/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, AllowAny
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -12,8 +12,7 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     permission_classes = [IsAdminUser]
 
-
-
-
-
-
+    def get_permissions(self):
+        if self.request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
+            return [permission() for permission in self.permission_classes]
+        return [AllowAny()]
